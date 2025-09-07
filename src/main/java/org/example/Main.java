e625a59e85110b4cc697eee3d50597b503636b39
IRPAN <a1234@Irpan-Mac.local>
2025-09-07 19:48:41 +0700

    Delete command guideline

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,8 @@ public class Main {
     public static void main(String[] args) {
         String bitNumberInput;
         if (args.length > 0) {
-            // Gunakan argumen dari command line jika ada
             bitNumberInput = args[0];
         } else {
-            // Jika tidak ada argumen, minta input dari user (untuk testing lokal)
             Scanner input = new Scanner(System.in);
             System.out.print("Please enter bits to convert: ");
             bitNumberInput = input.nextLine();
